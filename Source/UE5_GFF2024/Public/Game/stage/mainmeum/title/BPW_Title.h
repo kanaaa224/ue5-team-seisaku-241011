b737f40bfbfd384df7daaa376976e973f8d50250
issei-toyomoto <120348418+issei-toyomoto@users.noreply.github.com>
2024-10-18 13:49:27 +0900

    修正：コンパイルエラーする箇所を修正

--- Source/UE5_GFF2024/Public/Game/stage/mainmeum/title/BPW_Title.h
+++ Source/UE5_GFF2024/Public/Game/stage/mainmeum/title/BPW_Title.h
@@ -6,6 +6,8 @@
 #include "Blueprint/UserWidget.h"
 #include "BPW_Title.generated.h"
 
+class UButton;
+
 /**
  * 
  */
@@ -14,4 +16,22 @@ class UE5_GFF2024_API UBPW_Title : public UUserWidget
 {
 	GENERATED_BODY()
 	
+protected:
+	//NativeConstruct
+	void NativeConstruct() override;
+
+private:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> BottonPlay;
+
+	//ButtonPlayのOnClickedイベントに関連づける
+	UFUNCTION()
+	void OnButtonPlayClicked();
+
+	UPROPERTY(meta = (BindWidget))
+	class UButton* ButtonQuit;
+
+	// ButtonQuitのOnClickedイベントに関連づける
+	UFUNCTION()
+	void OnButtonQuitClicked();
 };
