b1c249bd52e2123bbd0b0dd9473cd0f59ba81381
issei-toyomoto <120348418+issei-toyomoto@users.noreply.github.com>
2024-10-18 10:36:27 +0900

    (create) C++のWBP_Titleを追加した

---
+++ Source/UE5_GFF2024/Public/Game/stage/mainmeum/title/BPW_Title.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "BPW_Title.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_GFF2024_API UBPW_Title : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
