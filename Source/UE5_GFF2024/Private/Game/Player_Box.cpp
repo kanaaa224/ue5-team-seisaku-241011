9c2b997bd1a1d67815b2ddf64bb9be3def0a8e57
nigirimeshi02 <104492636+nigirimeshi02@users.noreply.github.com>
2024-10-18 14:02:53 +0900

    プレイヤーの移動をさせようとしている、一旦保存

--- Source/UE5_GFF2024/Private/Game/Player_Box.cpp
+++ Source/UE5_GFF2024/Private/Game/Player_Box.cpp
@@ -2,6 +2,15 @@
 
 
 #include "Game/Player_Box.h"
+#include "Camera/CameraComponent.h"
+#include "Components/BoxComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/Controller.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
 
 // Sets default values
 APlayer_Box::APlayer_Box()
@@ -9,13 +18,58 @@ APlayer_Box::APlayer_Box()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	//スタティックメッシュを追加する
+	Cube = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMeshComponent"));
+	RootComponent = Cube;
+	UStaticMesh* mesh = LoadObject<UStaticMesh>(nullptr, TEXT("/Engine/BasicShapes/Cube"));
+	Cube->SetStaticMesh(mesh);
+
+	//マテリアルを追加する
+	UMaterial* material = LoadObject<UMaterial>(nullptr, TEXT("/Engine/BasicShapes/BasicShapeMaterial"));
+	Cube->SetMaterial(0, material);
+
+	Cube->SetSimulatePhysics(true);
+
+	//HitEventを有効にする
+	Cube->BodyInstance.bNotifyRigidBodyCollision = true;
+
+	//camera boomを追加する
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 400.0f;			//SpringArmの長さを調整する
+	CameraBoom->bUsePawnControlRotation = true; 	//PawnのControllerRotationを使用する
+
+	//follow cameraを追加する
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation = false; 	//PawnのControllerRotationを使用する
+
+	//IM_Defaultを読みこむ
+	DefaultMappingContext = LoadObject<UInputMappingContext>(nullptr, TEXT("/Game/ThirdPerson/Input/IMC_Default"));
+
+	//IA_Lookを読みこむ
+	BlinkAction = LoadObject<UInputAction>(nullptr, TEXT("/Game/ThirdPerson/Input/Actions/IA_Blink"));
+	//IA_Jumpを読み込む
+	MoveAction = LoadObject<UInputAction>(nullptr, TEXT("/Game/ThirdPerson/Input/Actions/IA_Move"));
+	//IA_Boostを読み込む
+	LookAction = LoadObject<UInputAction>(nullptr, TEXT("/Game/ThirdPerson/Input/Actions/IA_Look"));
+
 }
 
 // Called when the game starts or when spawned
 void APlayer_Box::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	//InputMappingContextの追加
+	if (const APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
+
 }
 
 // Called every frame
@@ -29,6 +83,71 @@ void APlayer_Box::Tick(float DeltaTime)
 void APlayer_Box::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	//アクションをバインドする
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent)) {
+
+		//Move
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &APlayer_Box::Move);
+
+		//Look
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &APlayer_Box::Look);
+
+		//Blink
+		EnhancedInputComponent->BindAction(BlinkAction, ETriggerEvent::Triggered, this, &APlayer_Box::Blink);
+	}
+}
+
+void APlayer_Box::Move(const FInputActionValue& Value)
+{
+	FVector2D MovementVector = Value.Get<FVector2D>();
+
+	if (Controller != nullptr)
+	{
+		//進行方向を探す
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		//前方向を取得
+		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+
+		//右方向を取得
+		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+		//動きを加える
+		AddMovementInput(ForwardDirection, MovementVector.Y);
+		AddMovementInput(RightDirection, MovementVector.X);
+
+		UE_LOG(LogTemp, Display, TEXT("Move"));
+	}
+}
+
+void APlayer_Box::Look(const FInputActionValue& Value)
+{
+	FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+	if (Controller != nullptr)
+	{
+		//ヨーとピッチの入力を加える
+		AddControllerYawInput(LookAxisVector.X);
+		AddControllerPitchInput(LookAxisVector.Y);
+	}
+}
+
+void APlayer_Box::Blink(const FInputActionValue& Value)
+{
+	//inputのvalueはboolに変換できる
+	if (const bool v = Value.Get<bool>())
+	{
+		//Arrowが向いている前方方向のVector情報を取得する
+		FVector ForwardVector = Cube->GetForwardVector().GetSafeNormal(0.0001f);
+
+		//torqueとして与えるVectorを作成する
+		FVector torqueVector = FVector(ForwardVector.Y * 1000000 * -1.f, ForwardVector.X * 1000000, 0.f);
+
+		//torqueを与えて加速する
+		Cube->AddTorqueInRadians(torqueVector, TEXT("None"), true);
+	}
 
 }
 
