394e074fb9a6f26567026b366a6fef8fb011bb7a
issei-toyomoto <120348418+issei-toyomoto@users.noreply.github.com>
2024-10-22 14:52:09 +0900

    [create & chage] create:AIController & chage:uproject
    作成したファイル
    ・Enemy2のAIController
    変更したファイル
    ・.uprojectファイル

---
+++ Source/UE5_GFF2024/Public/Game/Enemy/2/AIC_Enemy2.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "AIC_Enemy2.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_GFF2024_API AAIC_Enemy2 : public AAIController
+{
+	GENERATED_BODY()
+	
+};
