e02788c1903c90d25d2ad819ba18583a87a1c4e1
kanaaa224 <kanaaa224@outlook.com>
2024-11-05 14:56:17 +0900

    [ update ] タイトルウィジェット C++クラス 完成

--- Source/UE5_GFF2024/Public/Game/UI/Widget_TitleMenu.h
+++ Source/UE5_GFF2024/Public/Game/UI/Widget_TitleMenu.h
@@ -4,14 +4,29 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Components/Button.h"
 #include "Widget_TitleMenu.generated.h"
 
-/**
- * 
- */
+class UButton;
+
 UCLASS()
 class UE5_GFF2024_API UWidget_TitleMenu : public UUserWidget
 {
 	GENERATED_BODY()
 	
+protected:
+	void NativeConstruct() override;
+
+private:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> button_play;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> button_quit;
+
+	UFUNCTION()
+	void OnButtonPlayClicked();
+
+	UFUNCTION()
+	void OnButtonQuitClicked();
 };
