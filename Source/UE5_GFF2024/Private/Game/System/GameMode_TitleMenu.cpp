f7381543eb0ffea2ddc2d1c057bd04bd143773b9
kanaaa224 <kanaaa224@outlook.com>
2024-11-06 12:49:52 +0900

    [ update ] タイトル画面用 ゲームモードC++クラス 更新

--- Source/UE5_GFF2024/Private/Game/System/GameMode_TitleMenu.cpp
+++ Source/UE5_GFF2024/Private/Game/System/GameMode_TitleMenu.cpp
@@ -3,8 +3,12 @@
 
 #include "Game/System/GameMode_TitleMenu.h"
 #include "Game/UI/HUD_TitleMenu.h"
+#include "Game/Player_Cube.h"
 
 AGameMode_TitleMenu::AGameMode_TitleMenu()
 {
+	DefaultPawnClass = APlayer_Cube::StaticClass();
+	//DefaultPawnClass = nullptr;
+	//bStartPlayersAsSpectators = true;
 	HUDClass = AHUD_TitleMenu::StaticClass();
 }
