da7770611c408a9527869428c34c1553ae0ab139
moyakino <104491816+moyakino@users.noreply.github.com>
2024-11-08 15:00:02 +0900

    Enemy 3 の C++ 追加

---
+++ Source/UE5_GFF2024/Public/Game/Enemy/3/AIC_Enemy3.h
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "BehaviorTree/BehaviorTree.h"
+#include "BehaviorTree/BehaviorTreeComponent.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Game/Player_Cube.h"
+#include "AIC_Enemy3.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_GFF2024_API AAIC_Enemy3 : public AAIController
+{
+	GENERATED_BODY()
+	
+public:
+	AAIC_Enemy3(const class FObjectInitializer& ObjectInitializer);
+
+public:
+	void SetPlayerKey(APawn* player);
+
+	UFUNCTION()
+	APlayer_Cube* GetPlayerKey();
+
+	UPROPERTY()
+	UBehaviorTreeComponent* BehaviorComp;
+
+	UPROPERTY()
+	UBlackboardComponent* BlackboardComp;
+
+	UPROPERTY(EditDefaultsOnly, Category = AI)
+	FName PlayerKeyName;
+
+protected:
+	// AIControllerのPawn所持
+	virtual void OnPossess(class APawn* InPawn) override;
+
+	// AIControllerのPawn所持解除
+	virtual void OnUnPossess() override;
+
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly, Category = AI)
+	class UBehaviorTree* BehaviorTree;
+
+	FORCEINLINE UBehaviorTreeComponent* GetBehaviorComp() const { return BehaviorComp; }
+	FORCEINLINE UBlackboardComponent* GetBlackboardComp() const { return BlackboardComp; }
+};
