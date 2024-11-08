c51000f89f377b3c25503b5c475a75075879bd6d
kanaaa224 <kanaaa224@outlook.com>
2024-11-08 14:19:30 +0900

    [ update ] 残機 ウィジェットC++クラス 完成

--- Source/UE5_GFF2024/Private/Game/UI/Widget_PlayerLives.cpp
+++ Source/UE5_GFF2024/Private/Game/UI/Widget_PlayerLives.cpp
@@ -2,4 +2,55 @@
 
 
 #include "Game/UI/Widget_PlayerLives.h"
+#include "Game/System/GameInstance_GFF2024.h"
+#include "Kismet/GameplayStatics.h"
 
+void UWidget_PlayerLives::NativeConstruct()
+{
+	Super::NativeConstruct();
+}
+
+void UWidget_PlayerLives::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	Super::NativeTick(MyGeometry, InDeltaTime);
+
+	UGameInstance_GFF2024* GameInstance = Cast<UGameInstance_GFF2024>(UGameplayStatics::GetGameInstance(GetWorld()));
+
+	if (GameInstance && GameInstance->Life)
+	{
+		int32 lives = GameInstance->Life; // 残機数
+		int32 maxLives = 2;               // 残機最大数
+
+		// 残機の最大数に応じて残機の表示数を変える
+		if (maxLives < 3)
+		{
+			if (image_life_3) image_life_3->SetVisibility(ESlateVisibility::Collapsed);
+		}
+
+		if (maxLives < 2)
+		{
+			if (image_life_2) image_life_2->SetVisibility(ESlateVisibility::Collapsed);
+		}
+
+		if (maxLives < 1)
+		{
+			if (image_life_1) image_life_1->SetVisibility(ESlateVisibility::Collapsed);
+		}
+
+		// 残機数に応じて半透明にする
+		if (maxLives <= 3 && lives < 3)
+		{
+			if (image_life_3) image_life_3->SetColorAndOpacity(FLinearColor(1.0f, 1.0f, 1.0f, 0.1f));
+		}
+
+		if (maxLives <= 2 && lives < 2)
+		{
+			if (image_life_2) image_life_2->SetColorAndOpacity(FLinearColor(1.0f, 1.0f, 1.0f, 0.1f));
+		}
+
+		if (maxLives <= 1 && lives < 1)
+		{
+			if (image_life_1) image_life_1->SetColorAndOpacity(FLinearColor(1.0f, 1.0f, 1.0f, 0.1f));
+		}
+	}
+}
