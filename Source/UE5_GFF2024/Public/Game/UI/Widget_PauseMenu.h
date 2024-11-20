ac6ed69d82162dd0840009cbc276d9dfaac3c6ae
kanaaa224 <kanaaa224@outlook.com>
2024-11-20 13:44:25 +0900

    [ update ] ポーズ画面用 ウィジェットC++クラス 完成

--- Source/UE5_GFF2024/Public/Game/UI/Widget_PauseMenu.h
+++ Source/UE5_GFF2024/Public/Game/UI/Widget_PauseMenu.h
@@ -4,14 +4,37 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Components/Button.h"
 #include "Widget_PauseMenu.generated.h"
 
-/**
- * 
- */
+class UButton;
+
 UCLASS()
 class UE5_GFF2024_API UWidget_PauseMenu : public UUserWidget
 {
 	GENERATED_BODY()
-	
+
+protected:
+	void NativeConstruct() override;
+
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+
+private:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> button_close;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> button_title;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> button_quit;
+
+	UFUNCTION()
+	void onButtonCloseClicked();
+
+	UFUNCTION()
+	void onButtonTitleClicked();
+
+	UFUNCTION()
+	void onButtonQuitClicked();
 };
