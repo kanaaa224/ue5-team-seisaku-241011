e2d3eb3b488c1152ba3d3bf49518d31d08a8c4fe
moyakino <104491816+moyakino@users.noreply.github.com>
2024-11-26 14:51:33 +0900

    追加

--- Source/UE5_GFF2024/Private/Game/Enemy/3/BTT_Enemy3Attack1.cpp
+++ Source/UE5_GFF2024/Private/Game/Enemy/3/BTT_Enemy3Attack1.cpp
@@ -15,12 +15,18 @@ UBTT_Enemy3Attack1::UBTT_Enemy3Attack1()
 //タスクが動く この中に動かしたいことを書く
 EBTNodeResult::Type UBTT_Enemy3Attack1::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
+	// エラー（赤）
+	UE_LOG(LogTemp, Error, TEXT("BTT_Enemy3Attack1 ON"));
 
 	//AIコントローラーを取得
 	AAIController* AIController = OwnerComp.GetAIOwner();
 	if (AIController == nullptr) {
 		return EBTNodeResult::Failed;
 	}
+	/*else
+	{
+		UE_LOG(LogTemp, Error, TEXT("BTT_Enemy3Attack1 ON"));
+	}*/
 	//コントローた(AIC_Enemy2)を取得
 	AAIC_Enemy3* MyAIController = Cast<AAIC_Enemy3>(AIController);
 	if (MyAIController == nullptr) {
@@ -49,7 +55,17 @@ EBTNodeResult::Type UBTT_Enemy3Attack1::ExecuteTask(UBehaviorTreeComponent& Owne
 		return EBTNodeResult::Failed;
 	}
 
-	//MyPawn->BTT_TestLog();
+	//MyPawn->BTT_Enemy3Attack_Beam2();
+
+	//if (MyPawn->BTT_Enemy3Attack_Beam(MyPawn) == true)
+	//{
+	//	return EBTNodeResult::InProgress;
+	//}
+	//else
+	//{
+	//	//サクセスじゃないとError
+	//	return EBTNodeResult::Succeeded;
+	//}
 	
 
 	//サクセスじゃないとError
