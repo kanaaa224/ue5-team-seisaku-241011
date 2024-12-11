f67f514c30b75f0430e6b940ea8e180bcd15277e
gao2968 <118335646+gao2968@users.noreply.github.com>
2024-12-11 14:31:07 +0900

    senni

--- Source/UE5_GFF2024/Private/Game/UI/Widget_GameOver.cpp
+++ Source/UE5_GFF2024/Private/Game/UI/Widget_GameOver.cpp
@@ -2,4 +2,11 @@
 
 
 #include "Game/UI/Widget_GameOver.h"
+#include "Kismet/GameplayStatics.h"
 
+void UWidget_GameOver::OpenNextLevel()
+{
+	FTimerHandle TimerHandle;
+
+	GetWorld()->GetTimerManager().SetTimer(TimerHandle, [this]() {UGameplayStatics::OpenLevel(GetWorld(), "Level_TitleMenu"); }, 2.f, false);
+}
--- Source/UE5_GFF2024/Private/Game/UI/Widget_StageClear.cpp
+++ Source/UE5_GFF2024/Private/Game/UI/Widget_StageClear.cpp
@@ -2,4 +2,11 @@
 
 
 #include "Game/UI/Widget_StageClear.h"
+#include "Kismet/GameplayStatics.h"
 
+void UWidget_StageClear::OpenNextLevel()
+{
+	FTimerHandle TimerHandle;
+
+	GetWorld()->GetTimerManager().SetTimer(TimerHandle, [this]() {UGameplayStatics::OpenLevel(GetWorld(), "Stage2"); }, 2.f, false);
+}
